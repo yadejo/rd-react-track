2f00e53597de0740efc09f0cb1d80da0b0c8109b
Steven Van de Poel <stevenvandepoel@gmail.com>
2020-04-03 14:44:11 +0200

    default error catcher

--- api/index.js
+++ api/index.js
@@ -16,6 +16,23 @@ app.use((req, res, next) => {
     next()
 })
 
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+
+
 app.get('/pokemon', (req, res, next) => {
     pokemon = data.pokemon.map(({ id, url, sprite }) => { return { id, url, sprite } })
     if (pokemon) return res.json(pokemon)
