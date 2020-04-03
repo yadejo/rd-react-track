e87fa924a6cac28662c1a8cea2b506f4f81726d2
Steven Van de Poel <stevenvandepoel@gmail.com>
2020-04-03 15:48:24 +0200

    moved error middleware

--- api/index.js
+++ api/index.js
@@ -1,10 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const data = require('./pokemon.json')
 
 const port = 1337
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/pokemon', (req, res, next) => {
+    pokemon = data.pokemon.map(({ id, url, sprite }) => { return { id, url, sprite } })
+    if (pokemon) return res.json(pokemon)
+    return res.status(404).json({error:{message:'Not found'}})
+})
+
+app.get('/pokemon/:id', (req, res, next) => {
+    pokemon = data.pokemon.find(p => p.id == req.params.id)
+    if (pokemon) return res.json(pokemon)
+    return res.status(404).json({error:{message:'Not found'}})
+})
+
+app.use('/sprites', express.static('sprites'))
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
@@ -30,20 +47,6 @@ app.use((error, req, res, next) => {
         }
     })
 })
-
-app.get('/pokemon', (req, res, next) => {
-    pokemon = data.pokemon.map(({ id, url, sprite }) => { return { id, url, sprite } })
-    if (pokemon) return res.json(pokemon)
-    return res.sendStatus(404)
-})
-
-app.get('/pokemon/:id', (req, res, next) => {
-    pokemon = data.pokemon.find(p => p.id == req.params.id)
-    if (pokemon) return res.json(pokemon)
-    return res.sendStatus(404)
-})
-
-app.use('/sprites', express.static('sprites'))
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
